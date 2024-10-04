cb960fbb87ad37ada36e0211d224c48cf8f934f9
56@204 <56@204.school>
2024-10-04 16:24:20 +0900

    r6-10-4 204

---
+++ test.java
@@ -0,0 +1,10 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println("クラス：２B");
+		System.out.println("学生番号：23250012\n");
+		System.out.println("名前：コウヨウ");
+	}
+	
+}

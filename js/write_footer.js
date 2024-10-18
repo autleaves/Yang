9b5706b4cfb8fc1b3bd66016dfdd04f3476b44ef
Yeung <@Yeung>
2024-10-19 01:38:18 +0900

    test write header and footer by js 1:38

--- js/write_footer.js
+++ js/write_footer.js
@@ -3,7 +3,7 @@
 const footerElement = document.querySelector("footer");
 const fetchFooter = async () => {
   try {
-    const res = await fetch("footer.txt");
+    const res = await fetch("../modules/footer.txt");
     const template = await res.text();
 
     footerElement.innerHTML = template;
--- js/write_header.js
+++ js/write_header.js
@@ -3,7 +3,7 @@ const headerElement = document.querySelector("header");
 
 const fetchHeader = async () => {
   try {
-    const res = await fetch("header.txt");
+    const res = await fetch("../modules/header.txt");
     const template = await res.text();
 
     headerElement.innerHTML = template;

4c5055db988ceb7b3480767c9011ad6209a666bb
Yeung <@Yeung>
2024-10-19 01:56:16 +0900

    test write header and footer by js 1:56

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,6 @@
 const nav_menuButton = document.querySelector('#toggle-menu');
 nav_menuButton.addEventListener('click', function(event) {
     event.preventDefault();
-    const menu = document.getElementById('main-menu');
+    const menu = document.querySelector('#main-menu');
     menu.classList.toggle('is-open');
 });

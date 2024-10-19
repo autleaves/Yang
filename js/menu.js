17c431d86b02adc457611225cb8d75c0d5e06fe4
Yeung <@Yeung>
2024-10-19 10:13:55 +0900

    test write header and footer by js 10:13

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,2 @@
 
 
-const nav_menuButton = document.querySelector('#toggle-menu');
-nav_menuButton.addEventListener('click', function(event) {
-    event.preventDefault();
-    const menu = document.querySelector('#main-menu');
-    menu.classList.toggle('is-open');
-});
--- js/write_footer.js
+++ js/write_footer.js
@@ -12,3 +12,33 @@ const fetchFooter = async () => {
   }
 };
 fetchFooter();
+
+const nav_menuButton = document.querySelector('#toggle-menu');
+nav_menuButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    const menu = document.querySelector('#main-menu');
+    menu.classList.toggle('is-open');
+});
+
+const search_input = document.querySelector(".search-input");
+
+const search_handleInputChange = (e) => {
+    if (e.target.value && !search_input.classList.contains("search--touched")) {
+        search_input.classList.add("search--touched");
+    } else if (!e.target.value && search_input.classList.contains("search--touched")) {
+        search_input.classList.remove("search--touched");
+    }
+}
+search_input.addEventListener("input", search_handleInputChange);
+
+const search_toggle = document.querySelector('#search__menu-toggle');
+search_toggle.addEventListener('focus', function(event) {
+    event.preventDefault();
+    const menu = document.querySelector('#search__menu-list');
+    menu.style.display = "block";
+});
+search_toggle.addEventListener('blur', function(event) {
+    event.preventDefault();
+    const menu = document.querySelector('#search__menu-list');
+    menu.style.display = "none";
+});

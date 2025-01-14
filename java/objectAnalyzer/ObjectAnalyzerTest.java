c92e82e89f3c87f122b6b43d67db1512f8e4038e
56@204 <56@204.school>
2025-01-14 11:12:43 +0900

    204

--- java/objectAnalyzer/ObjectAnalyzerTest.java
+++ java/objectAnalyzer/ObjectAnalyzerTest.java
@@ -1,14 +1,17 @@
 package objectAnalyzer;
+
+import java.util.ArrayList;
 import static java.lang.System.out;
+
 public class ObjectAnalyzerTest {
 
     public static void main(String[] args)
         throws ReflectiveOperationException
     {
-        var squares = new ArrayList<Integer>();
+        ArrayList<Integer> squares = new ArrayList<>();
         for(int i=1; i <= 5; i++){
             squares.add(i * i);
         }
-    
+        out.println(new ObjectAnalyzer().toString(squares));
     }
 }
